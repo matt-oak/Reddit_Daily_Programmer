7d4170d7e665c098240c479618c6de8744811b05
matt-oak <matt.oak@live.com>
2016-06-07 11:44:57 -0600

    Solution to Challenge #2 [Hard]

---
+++ Challenge_2_Hard.py
@@ -0,0 +1,34 @@
+#/r/DailyProgrammer Challenge #2 [Hard]
+#Stopwatch program
+
+import time
+
+print "Basic stopwatch program"
+init_input = raw_input("Enter [s]tart to begin stopwatch: ")
+
+if init_input == "s":
+	print "Enter [l] to lap or [e] to stop the stopwatch"
+	log_file = open("Stopwatch_Info.txt", "w")
+	start = time.time()
+	num_laps = 0
+	while True:
+		input = raw_input()
+		if input == "l":
+			num_laps = num_laps + 1
+			lap = time.time()
+			dif = str(lap - start)
+			dif = dif[:5]
+			print "Lap #" + str(num_laps) + ":", dif, "seconds"
+			log_file.write("Lap #%d: %s seconds\n" % (num_laps, dif))
+		elif input == "e":
+			end = time.time()
+			dif = str(end - start)
+			dif = dif[:5]
+			print "Total time:", dif, "seconds"
+			log_file.write("Total time: %s seconds" % dif)
+			exit()
+		else:
+			print "Invalid input, please enter [l] for lap or [e] to stop"
+			
+else:
+	print "Invalid input"
